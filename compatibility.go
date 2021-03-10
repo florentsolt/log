6122886f6278e41c1734f2179577d213fade151c
Florent Solt <florent@solt.biz>
2021-03-10 15:35:28 +0100

    Trim

--- compatibility.go
+++ compatibility.go
@@ -23,55 +23,55 @@ func (c CompatibilityWrapper) V(level int) bool { return true }
 
 // debug
 func (c CompatibilityWrapper) Debug(args ...interface{}) {
-	c.wrapper.Debug().Msg(strings.TrimSuffix(fmt.Sprint(args...), "\n"))
+	c.wrapper.Debug().Msg(strings.TrimSpace(fmt.Sprint(args...)))
 }
 func (c CompatibilityWrapper) Debugln(args ...interface{}) {
-	c.wrapper.Debug().Msg(fmt.Sprint(args...))
+	c.wrapper.Debug().Msg(strings.TrimSpace(fmt.Sprint(args...)))
 }
 func (c CompatibilityWrapper) Debugf(format string, args ...interface{}) {
-	c.wrapper.Debug().Msgf(format, args...)
+	c.wrapper.Debug().Msgf(strings.TrimSpace(fmt.Sprintf(format, args...)))
 }
 
 // info
 func (c CompatibilityWrapper) Info(args ...interface{}) {
-	c.wrapper.Info().Msg(strings.TrimSuffix(fmt.Sprint(args...), "\n"))
+	c.wrapper.Info().Msg(strings.TrimSpace(fmt.Sprint(args...)))
 }
 func (c CompatibilityWrapper) Infoln(args ...interface{}) {
-	c.wrapper.Info().Msg(fmt.Sprint(args...))
+	c.wrapper.Info().Msg(strings.TrimSpace(fmt.Sprint(args...)))
 }
 func (c CompatibilityWrapper) Infof(format string, args ...interface{}) {
-	c.wrapper.Info().Msgf(format, args...)
+	c.wrapper.Info().Msgf(strings.TrimSpace(fmt.Sprintf(format, args...)))
 }
 
 // warning
 func (c CompatibilityWrapper) Warning(args ...interface{}) {
-	c.wrapper.Warn().Caller(1).Msg(strings.TrimSuffix(fmt.Sprint(args...), "\n"))
+	c.wrapper.Warn().Msg(strings.TrimSpace(fmt.Sprint(args...))
 }
 func (c CompatibilityWrapper) Warningln(args ...interface{}) {
-	c.wrapper.Warn().Msg(fmt.Sprint(args...))
+	c.wrapper.Warn().Msg(strings.TrimSpace(fmt.Sprint(args...)))
 }
 func (c CompatibilityWrapper) Warningf(format string, args ...interface{}) {
-	c.wrapper.Warn().Msgf(format, args...)
+	c.wrapper.Warn().Msgf(strings.TrimSpace(fmt.Sprintf(format, args...)))
 }
 
 // error
 func (c CompatibilityWrapper) Error(args ...interface{}) {
-	c.wrapper.Error().Msg(strings.TrimSuffix(fmt.Sprint(args...), "\n"))
+	c.wrapper.Error().Msg(strings.TrimSpace(fmt.Sprint(args...))
 }
 func (c CompatibilityWrapper) Errorln(args ...interface{}) {
-	c.wrapper.Error().Msg(fmt.Sprint(args...))
+	c.wrapper.Error().Msg(strings.TrimSpace(fmt.Sprint(args...)))
 }
 func (c CompatibilityWrapper) Errorf(format string, args ...interface{}) {
-	c.wrapper.Error().Msgf(format, args...)
+	c.wrapper.Error().Msgf(strings.TrimSpace(fmt.Sprintf(format, args...)))
 }
 
 // fatal
 func (c CompatibilityWrapper) Fatal(args ...interface{}) {
-	c.wrapper.Fatal().Msg(strings.TrimSuffix(fmt.Sprint(args...), "\n"))
+	c.wrapper.Fatal().Msg(strings.TrimSpace(fmt.Sprint(args...))
 }
 func (c CompatibilityWrapper) Fatalln(args ...interface{}) {
-	c.wrapper.Fatal().Msg(fmt.Sprint(args...))
+	c.wrapper.Fatal().Msg(strings.TrimSpace(fmt.Sprint(args...)))
 }
 func (c CompatibilityWrapper) Fatalf(format string, args ...interface{}) {
-	c.wrapper.Fatal().Msgf(format, args...)
+	c.wrapper.Fatal().Msgf(strings.TrimSpace(fmt.Sprintf(format, args...)))
 }
